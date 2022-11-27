80e064d950b233df3374aa54e70e264609f7b9af
JMOrbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2022-11-27 08:49:50 -0500

    Add Git parameter on Invoke-Dotfiles-Create

--- PSWindowsDotfiles/Functions/Invoke-Dotfiles-Create.ps1
+++ PSWindowsDotfiles/Functions/Invoke-Dotfiles-Create.ps1
@@ -8,8 +8,14 @@
  .Parameter Path
   Path of the Dotfiles project, must not contain a folder named "Dotfiles".
 
+ .Parameter Git
+  Initialize a Git repository in the directory.
+
   .Example
   Invoke-Dotfiles-Create -Path "%USERPROFILE%";
+
+  .Example
+  Invoke-Dotfiles-Create -Path "%USERPROFILE%" -Git;
  .
 #>
 function Invoke-Dotfiles-Create {
@@ -25,7 +31,14 @@ function Invoke-Dotfiles-Create {
         return $TRUE;
       })]
     [System.IO.FileInfo]
-    $Path
+    $Path,
+    [ValidateScript({        
+        if (-Not (Get-Command -Name "git" -ErrorAction "SilentlyContinue")) { 
+          throw "Git must be installed and in your PATH";
+        }
+        return $TRUE;
+      })]
+    [Switch] $Git
   )
   
   Process {
@@ -34,5 +47,16 @@ function Invoke-Dotfiles-Create {
 
     # Copy the Dotfiles structure to the new Dotfiles directory
     Copy-Item -Path "$PSScriptRoot\..\DotfilesStructure\*" -Destination "$Path\Dotfiles" -Recurse;
+
+    if ($Git.IsPresent) {
+      # Init Git repo on the Dotfiles folder
+      git init "$Path\Dotfiles";
+
+      # Add changes
+      git -C "$Path\Dotfiles" add .;
+
+      # Make Git commit on the Dotfiles
+      git -C "$Path\Dotfiles" commit -m "Initial commit";
+    }
   }
 }
