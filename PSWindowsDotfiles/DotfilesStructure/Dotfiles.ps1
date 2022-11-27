8acb0effb7e61b2170182466085f422dc0763842
JMOrbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2022-11-27 08:51:21 -0500

    Fix case in Dotfiles.ps1

--- PSWindowsDotfiles/DotfilesStructure/Dotfiles.ps1
+++ PSWindowsDotfiles/DotfilesStructure/Dotfiles.ps1
@@ -1,17 +1,17 @@
 # Install & configure base apps
-Invoke-Expression "$env:DOTFILES_PATH\base\base.ps1";
+Invoke-Expression "$env:DOTFILES_PATH\Base\Base.ps1";
 
 # Install & configure programming languages
-Invoke-Expression "$env:DOTFILES_PATH\langs\langs.ps1";
+Invoke-Expression "$env:DOTFILES_PATH\Langs\Langs.ps1";
 
 # Install & configure apps
-Invoke-Expression "$env:DOTFILES_PATH\apps\install.ps1";
+Invoke-Expression "$env:DOTFILES_PATH\Apps\Install.ps1";
 
 # Update & configure apps
-Invoke-Expression "$env:DOTFILES_PATH\apps\update.ps1";
+Invoke-Expression "$env:DOTFILES_PATH\Apps\Update.ps1";
 
 # Configure the Operating System
-Invoke-Expression "$env:DOTFILES_PATH\os\os.ps1";
+Invoke-Expression "$env:DOTFILES_PATH\OS\OS.ps1";
 
 # Clean Dotfiles temp folder & clean the Operating System
-Invoke-Expression "$env:DOTFILES_PATH\clean\clean.ps1";
+Invoke-Expression "$env:DOTFILES_PATH\Clean\Clean.ps1";
