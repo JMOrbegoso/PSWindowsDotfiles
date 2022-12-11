2e138c84cb5a7a70c03a296bd1bf606acfe0470c
JMOrbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2022-12-11 13:15:17 -0500

    Update Disable-WindowsFeature

--- PSWindowsDotfiles/Helpers/Disable-WindowsFeature.ps1
+++ PSWindowsDotfiles/Helpers/Disable-WindowsFeature.ps1
@@ -3,18 +3,8 @@ function Disable-WindowsFeature {
   param(
     [Parameter(Mandatory = $TRUE)]
     [String]
-    $FeatureKey,
-
-    [Parameter(Mandatory = $TRUE)]
-    [String]
-    $FeatureName
+    $FeatureKey
   )
   
-  if (Test-WindowsFeature -FeatureKey $FeatureKey) {
-    Write-Host "Disabling" $FeatureName ":" -ForegroundColor "Green";
-    Disable-WindowsOptionalFeature -FeatureName $FeatureKey -Online -NoRestart;
-  }
-  else {
-    Write-Host $FeatureName "is already disabled." -ForegroundColor "Green";
-  }
+  Disable-WindowsOptionalFeature -FeatureName $FeatureKey -Online -NoRestart;
 }
