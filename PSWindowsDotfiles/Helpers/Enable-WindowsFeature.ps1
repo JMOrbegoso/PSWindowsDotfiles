36676ba4cdf848b8f6ec0e90b725b9b41b420435
JMOrbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2022-12-11 13:14:22 -0500

    Update Enable-WindowsFeature

--- PSWindowsDotfiles/Helpers/Enable-WindowsFeature.ps1
+++ PSWindowsDotfiles/Helpers/Enable-WindowsFeature.ps1
@@ -3,18 +3,8 @@ function Enable-WindowsFeature {
   param(
     [Parameter(Mandatory = $TRUE)]
     [String]
-    $FeatureKey,
-
-    [Parameter(Mandatory = $TRUE)]
-    [String]
-    $FeatureName
+    $FeatureKey
   )
 
-  if (-not (Test-WindowsFeature -FeatureKey $FeatureKey)) {
-    Write-Host "Enabling" $FeatureName ":" -ForegroundColor "Green";
-    Enable-WindowsOptionalFeature -FeatureName $FeatureKey -Online -All -NoRestart;
-  }
-  else {
-    Write-Host $FeatureName "is already enabled." -ForegroundColor "Green";
-  }
+  Enable-WindowsOptionalFeature -FeatureName $FeatureKey -Online -All -NoRestart;
 }
